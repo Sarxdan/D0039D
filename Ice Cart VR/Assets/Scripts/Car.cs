f785cf6c358ba8600c22c93ca3d025a45dc7a7d2
sanank-7 <sanank-7@student.ltu.se>
2018-11-07 10:29:56 +0100

    pre update

---
+++ Ice Cart VR/Assets/Scripts/Car.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+    public float weight;
+    public Wheel wheel;
+
+	// Use this for initialization
+	void Start () {
+        wheel = this.gameObject.transform.GetComponentInChildren<Wheel>();
+	}
+
+    void FixedUpdate()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
