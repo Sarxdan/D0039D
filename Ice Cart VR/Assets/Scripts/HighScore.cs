9f974c5a36330ba94fbeb2d5aaeaa2f2ccd6aef4
Kristoffer Lundgren <klu0711@gmail.com>
2018-11-27 16:36:07 +0100

    added script for highscore

---
+++ Ice Cart VR/Assets/Scripts/HighScore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour {
+
+    Dictionary<string, int> highScore;
+
+
+	// Use this for initialization
+	void Start () {
+
+        for (int i = 0; i < 5; i++)
+        {
+
+        }
+        
+	}
+
+    void addScore(string name, int time)
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
