b011bffd79c7d5e0c211979fbd213185b54ff3fc
Benjamin Vesterlund <benves-7@student.ltu.se>
2018-11-30 14:41:37 +0100

    input fix

--- Ice Cart VR/Assets/Scripts/Script.cs
+++ Ice Cart VR/Assets/Scripts/Script.cs
@@ -11,9 +11,11 @@ public class Script : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
+        
         InputModule = this.gameObject.GetComponent<StandaloneInputModule>();
         // sets the input type to the first type of controller. (based on order in the machine) 
         string[] names = Input.GetJoystickNames();
+        Debug.Log(names[0]);
         if (names.Length == 0)
         {
             InputModule.horizontalAxis = "KeyboardHorizontal";
@@ -24,18 +26,21 @@ public class Script : MonoBehaviour {
 
         for (int i = 0; i < names.Length; i++)
         {
+            Debug.Log("HEJ");
             // SteeringWheel
             if (names[i].Equals("G29 Driving Force Racing Wheel"))
             {
-                InputModule.horizontalAxis  = "SteeringwheelHorizontal";
-                InputModule.verticalAxis    = "SteeringwheelVertical";
-                InputModule.submitButton    = "SteeringwheelSubmit";
-                InputModule.cancelButton    = "SteeringwheelBack";
+                Debug.Log("HEJ");
+                InputModule.horizontalAxis = "SteeringwheelHorizontal";
+                InputModule.verticalAxis = "SteeringwheelVertical";
+                InputModule.submitButton = "SteeringwheelSubmit";
+                InputModule.cancelButton = "SteeringwheelBack";
                 break;
             }
             // Xbox Controller
-            else if (names[i].Equals("Xbox One For Windows"))
+            else if (names[i].Equals("Xbox One For Windows") || names[i].Equals("Controller (XBOX 360 For Windows)"))
             {
+
                 InputModule.horizontalAxis  = "XboxHorizontal";
                 InputModule.verticalAxis    = "XboxVertical";
                 InputModule.submitButton    = "XboxSubmit";
@@ -50,6 +55,13 @@ public class Script : MonoBehaviour {
                 InputModule.submitButton    = "Ps4Submit";
                 InputModule.cancelButton    = "Ps4Back";
                 break;
+            }
+            else
+            {
+                InputModule.horizontalAxis = "KeyboardHorizontal";
+                InputModule.verticalAxis = "KeyboardVertical";
+                InputModule.submitButton = "KeyboardSubmit";
+                InputModule.cancelButton = "KeyboardBack";
             }
         }
     }
